71e99c1c35697a1d0375f877ee1d6ba402699eb8
imontecalvo <imontecalvo@fi.uba.ar>
2023-06-12 13:30:45 -0300

    modelo de proyecto actualizado

--- models/project.py
+++ models/project.py
@@ -1,20 +1,41 @@
 from utils.db import db
 import datetime as dt
 
-class Project(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    cost = db.Column(db.Integer)
-    created_at = db.Column(db.DateTime(), default=dt.datetime.now())
 
-    def __init__(self, name, cost):
-        self.name = name
-        self.cost = cost
+class Project(db.Model):
+    codigo = db.Column(db.Integer, primary_key=True)
+    id_cliente = db.Column(db.Integer)
+    id_producto = db.Column(db.Integer)
+    version = db.Column(db.String(10))
+    customizacion = db.Column(db.String(60))
+    nombre = db.Column(db.String(60))
+    fecha_inicio = db.Column(db.DateTime(), default=dt.datetime.now())
+    fecha_fin_estimada = db.Column(db.DateTime())
+    estado = db.Column(db.String(20), default="iniciado")
+    horas_consumidas = db.Column(db.Integer)
+    costo = db.Column(db.Integer)
+
+    def __init__(self, data):
+        self.id_cliente = data["id_cliente"]
+        self.id_producto = data["id_producto"]
+        self.version = data["version"]
+        self.customizacion = data["customizacion"]
+        self.nombre = data["nombre"]
+        self.fecha_fin_estimada = data["fecha_fin_estimada"]
+        self.horas_consumidas = data["horas_consumidas"]
+        self.costo = data["costo"]
 
     def to_dict(self):
         return {
-            "id":self.id,
-            "name":self.name,
-            "cost":self.cost,
-            "created_at":self.created_at
+            "codigo": self.codigo,
+            "id_cliente": self.id_cliente,
+            "id_producto": self.id_producto,
+            "version": self.version,
+            "customizacion": self.customizacion,
+            "nombre": self.nombre,
+            "fecha_inicio": self.fecha_inicio,
+            "fecha_fin_estimada": self.fecha_fin_estimada,
+            "estado": self.estado,
+            "horas_consumidas": self.horas_consumidas,
+            "costo": self.costo,
         }
