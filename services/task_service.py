3afc150009b1f35201c7b451444cd515bd1919b3
acevedoanalia <naacevedo@fi.uba.ar>
2023-06-14 21:42:44 -0300

    service_task

---
+++ services/task_service.py
@@ -0,0 +1,28 @@
+from utils.db import db    
+from models.task import Task
+
+def add_task(task):
+    db.session.add(task)
+    db.session.commit()
+
+def get_all_tasks():
+    return Task.query.all()
+
+def get_task(id):
+    return Task.query.get(id)
+
+def delete_task(id):
+    task = Task.query.get(id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+        return True
+    False
+
+def update_task(id, data):
+    task = Task.query.get(id)
+    if task:
+        task.update_data(data)
+        db.session.commit()
+        return task
+    return False
